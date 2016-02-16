5868c1b8d7e7c3a39f5c9458a6bcc08bd571744f
Colombo Simone <simone.colombo@mis.mediobanca.com>
2016-02-16 15:15:45 +0100

    Corretto contesto di lancio del comando 'cmk'

--- installsite.sh
+++ installsite.sh
@@ -32,7 +32,7 @@ if [ ${DRYRUN} -eq 0 ]; then
     # NOTE: this depends on the exported variables above
 
     # Determine the running version of CheckMK
-    export CMK_PKG_VERSION=$(cmk --version | grep "check_mk version" | awk '{print $NF}')
+    export CMK_PKG_VERSION=$(su - ${SITE} -c "cmk --version" | grep "check_mk version" | awk '{print $NF}')
 
     ${SOURCEDIR}/scripts/create_package_descriptor.sh
     cp ${SOURCEDIR}/.cmkpackage.json ${OMDBASE}/${SITE}/var/check_mk/packages/${NAME}
